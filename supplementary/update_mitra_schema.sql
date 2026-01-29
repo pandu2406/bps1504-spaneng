d2211cff7640bc4fe7119b620fe7905ff2dda95e
pandu2406 <dinata240600@gmail.com>
2026-01-29 11:13:05 +0700

    Remove sensitive .sql files from repository

--- supplementary/update_mitra_schema.sql
+++
@@ -1,6 +0,0 @@
--- Add 'tahun' column to mitra table
--- Default to 2025 for existing records
-ALTER TABLE mitra ADD COLUMN tahun INT(4) NOT NULL DEFAULT 2025 AFTER sobat_id;
-
--- Ensure we can have multiple entries for the same NIK/SOBAT ID if needed in different years?
--- For now, just adding the column is safe.
